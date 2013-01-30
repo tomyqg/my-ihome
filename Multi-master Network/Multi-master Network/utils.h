069b15fc820bf8891042b9130d8ec1572076ee99
t.fidecki <t.fidecki@gmail.com>
2013-01-30 18:28:37 +0000

    Utilities. NOP and memory barrier implementation.

---
+++ Multi-master Network/Multi-master Network/utils.h
@@ -0,0 +1,38 @@
+/*
+ * utils.h
+ *
+ * Created: 2013-01-30 17:56:03
+ *  Author: fidectom
+ */ 
+
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+/**
+   \def _NOP
+
+   Execute a <i>no operation</i> (NOP) CPU instruction.  This
+   should not be used to implement delays, better use the functions
+   from <util/delay_basic.h> or <util/delay.h> for this.  For
+   debugging purposes, a NOP can be useful to have an instruction that
+   is guaranteed to be not optimized away by the compiler, so it can
+   always become a breakpoint in the debugger.
+*/
+#define _NOP() __asm__ __volatile__("nop":::"memory") 
+
+/**
+   \def _MemoryBarrier
+
+   Implement a read/write <i>memory barrier</i>.  A memory
+   barrier instructs the compiler to not cache any memory data in
+   registers beyond the barrier.  This can sometimes be more effective
+   than blocking certain optimizations by declaring some object with a
+   \c volatile qualifier.
+
+   See \ref optim_code_reorder for things to be taken into account
+   with respect to compiler optimizations.
+*/
+#define _MemoryBarrier() __asm__ __volatile__("":::"memory")
+
+#endif /* UTILS_H_ */
